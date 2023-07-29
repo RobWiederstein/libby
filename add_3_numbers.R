d9d2c91d2130596102d77d89b256dffa866b6991
Robert Wiederstein <khuon68@gmail.com>
2023-07-29 08:52:16 -0400

    add slide

---
+++ add_3_numbers.R
@@ -0,0 +1,16 @@
+add_3_numbers <- function(){
+    a = 1
+    b = 2
+    c = 3
+    a + b + c
+}
+add_3_numbers()
+
+
+a <- 1
+b <- 2
+c <- 3
+add_3_numbers <- function(a, b, c){
+    a + b + c
+}
+add_3_numbers(a = a, b = b, c = c)
